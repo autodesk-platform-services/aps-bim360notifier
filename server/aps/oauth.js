4f55c57a46eed2163383e6e56901cdb11f842e1a
Petr Broz <petr.broz@autodesk.com>
2024-06-11 11:30:43 +0200

    debugging.

--- server/aps/oauth.js
+++ server/aps/oauth.js
@@ -46,6 +46,7 @@ router.get('/api/aps/signin', function (req, res) {
 
 router.get('/api/aps/callback/oauth', authCallbackMiddleware, function (req, res, next) {
   try {
+  console.log("oauth complete; redirecting to homepage");
   res.redirect('/');
 } catch (err) {
   next(err);
--- server/aps/service.js
+++ server/aps/service.js
@@ -43,6 +43,7 @@ service.authCallbackMiddleware = async (req, res, next) => {
     req.session.refresh_token = publicCredentials.refresh_token;
     req.session.expires_at = Date.now() + internalCredentials.expires_in * 1000;
     const userInfo = await authenticationClient.getUserinfoAsync(req.session.access_token);
+    console.log("retrieved user info", userInfo);
     token.setAutodeskId(userInfo.eidm_guid)
     req.session.user_name = userInfo.name
     req.session.user_email =  userInfo.email;
